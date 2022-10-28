837f3564f96f928c01426ad2f3d4353b36a2ac91
ben0588 <energy95272z@gmail.com>
2022-10-28 17:06:39 +0800

    更新 scss 架構、修改助教建議項目

--- js/all.js
+++ js/all.js
@@ -1,7 +1,14 @@
-AOS.init();
+AOS.init()
 
-const body = document.querySelector('body');
-const toggleBtn = document.querySelector('.toggle-menu');
-toggleBtn.addEventListener('click', function () { 
-    body.classList.toggle('show-menu');
+const container = document.querySelector('.container')
+const toggleBtn = document.querySelector('.toggle-menu')
+toggleBtn.addEventListener('click', function () {
+    container.classList.toggle('show-menu')
 })
+
+// const root = document.documentElement
+// const toggle_theme = document.querySelector('.toggle-theme')
+// toggle_theme.addEventListener('click', function () {
+//     console.log(root)
+//     root.style.setProperty('$title-color', '#74b9ff')
+// })
